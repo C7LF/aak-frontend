1933fc39bd7c4bc22afcbbc46af66a07cf6c653d
C7LF <ccal72@yahoo.co.uk>
2021-04-26 22:55:38 +0100

    removed fallback so pages 404

--- pages/projects/[projectSlug].tsx
+++ pages/projects/[projectSlug].tsx
@@ -39,7 +39,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
   return {
     paths,
-    fallback: true,
+    fallback: false,
   };
 };
 
@@ -52,38 +52,40 @@ const newLineText = (text: string) => {
 };
 
 const Project: React.FC<ProjectProps> = ({ project }) => {
-  return (
-    <Layout varient="full">
-      <div className="container mx-auto">
-        <Breadcrumbs />
-      </div>
-      <div className="md:flex">
-        <div className="md:w-1/2 h-pi bg-gray-100">
-          <div className="container inset-center mt-20">
-            <div className="md:w-1/2 md:pr-12">
-              <h1 className="text-5xl font-semibold mb-2">{project.title}</h1>
-              <p className="font-light text-lg">{project.heroContent}</p>
+  if (project) {
+    return (
+      <Layout varient="full">
+        <div className="container mx-auto">
+          <Breadcrumbs />
+        </div>
+        <div className="md:flex">
+          <div className="md:w-1/2 h-pi bg-gray-100">
+            <div className="container inset-center mt-20">
+              <div className="md:w-1/2 md:pr-12">
+                <h1 className="text-5xl font-semibold mb-2">{project.title}</h1>
+                <p className="font-light text-lg">{project.heroContent}</p>
+              </div>
             </div>
           </div>
+          <div className="md:w-1/2 h-pi relative">
+            <Image
+              src={`${process.env.NEXT_PUBLIC_API_URL}${project.image.url}`}
+              alt={project.image.alternativeText}
+              loading="lazy"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
         </div>
-        <div className="md:w-1/2 h-pi relative">
-          <Image
-            src={`${process.env.NEXT_PUBLIC_API_URL}${project.image.url}`}
-            alt={project.image.alternativeText}
-            loading="lazy"
-            layout="fill"
-            objectFit="cover"
-          />
+        <div className="container mx-auto relative">
+          {newLineText(project.content)}
+          {project.fancyGallery && (
+            <FancyGallery fancyItems={project.fancyGallery} />
+          )}
         </div>
-      </div>
-      <div className="container mx-auto relative">
-        {newLineText(project.content)}
-        {project.fancyGallery && (
-          <FancyGallery fancyItems={project.fancyGallery} />
-        )}
-      </div>
-    </Layout>
-  );
+      </Layout>
+    );
+  }
 };
 
 export default Project;
