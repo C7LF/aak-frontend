127ffc3d005c9fbc736b0dee2d8edd3dba38aafa
C7LF <ccal72@yahoo.co.uk>
2021-04-26 23:04:58 +0100

    chaning null values

--- pages/about.tsx
+++ pages/about.tsx
@@ -13,7 +13,7 @@ export const getStaticProps: GetStaticProps = async () => {
 
   return {
     props: {
-      content: data.about.content,
+      content: data?.about?.content,
     },
     revalidate: 1,
   };
